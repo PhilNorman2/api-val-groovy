c4f9912d4e5caf1cb52459af9066b5ab24ee7299
Norman <norman@oclc.org>
2017-10-19 14:25:46 -0400

    Initial commit of api-val-groovy

--- gitDeploy.sh
+++ gitDeploy.sh
@@ -4,8 +4,8 @@ git init
 git add .
 git commit -m "Initial commit of api-val-groovy"
 #create origin first time through
-#git remote add origin https://github.com/PhilNorman2/api-val-groovy
+git remote add origin https://github.com/PhilNorman2/api-val-groovy
 #use existing origin -after first time through
-git remote set-url origin https://github.com/PhilNorman2/api-validation
+#git remote set-url origin https://github.com/PhilNorman2/api-val-groovy
 git push -f origin master
 
